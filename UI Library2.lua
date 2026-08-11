0307b4f778a21aaee036e5a49d326a589c1e2920
SecoDevYT <butterflydemon35@googlemail.com>
2026-08-12 00:41:53 +0200

    Rename UI Library.lua to UI Library2.lua

--- UI Library2.lua
+++ UI Library2.lua
@@ -9,7 +9,7 @@ local function makeDraggable(frame)
 	local startPos
 	local currentPos
 
-	local smoothness = 0.07 -- lower = snappier, higher = softer
+	local smoothness = 0.075 -- lower = snappier, higher = softer
 
 	frame.InputBegan:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
@@ -48,34 +48,45 @@ end
 function Library:CreateWindow(title)
 	local ScreenGui = Instance.new("ScreenGui")
 	local Frame = Instance.new("Frame")
-	local UICorner = Instance.new("UICorner")
 	local TextLabel = Instance.new("TextLabel")
+	local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+	local UICorner = Instance.new("UICorner")
 
-	ScreenGui.Parent = game:GetService("CoreGui")
+	-- ScreenGui
+	ScreenGui.Parent = game:GetService("CoreGui") -- change to PlayerGui if you prefer
 	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 	ScreenGui.ResetOnSpawn = false
+	ScreenGui.Name = "MonstrumUI"
 
+	-- Main Frame
 	Frame.Parent = ScreenGui
 	Frame.BackgroundColor3 = Color3.fromRGB(18, 18, 18)
 	Frame.BorderSizePixel = 0
-	Frame.Position = UDim2.new(0.5, -196, 0.5, -188)
-	Frame.Size = UDim2.new(0, 392, 0, 376)
+	Frame.Position = UDim2.new(0.324, 0, 0.222, 0)
+	Frame.Size = UDim2.new(0.365, 0, 0.554, 0)
 	Frame.Active = true
 
 	-- Soft drag
 	makeDraggable(Frame)
 
-	UICorner.CornerRadius = UDim.new(0, 10)
+	-- Aspect Ratio
+	UIAspectRatioConstraint.Parent = Frame
+	UIAspectRatioConstraint.AspectRatio = 1.043
+
+	-- Corner
+	UICorner.CornerRadius = UDim.new(0.04, 0)
 	UICorner.Parent = Frame
 
+	-- Title
 	TextLabel.Parent = Frame
 	TextLabel.BackgroundTransparency = 1
-	TextLabel.Position = UDim2.new(0, 12, 0, 8)
-	TextLabel.Size = UDim2.new(1, -24, 0, 24)
+	TextLabel.Position = UDim2.new(0.015, 0, 0.01, 0)
+	TextLabel.Size = UDim2.new(0.745, 0, 0.061, 0)
 	TextLabel.FontFace = Font.fromName("Montserrat", Enum.FontWeight.ExtraBold)
 	TextLabel.Text = title or "Window"
 	TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabel.TextSize = 16
+	TextLabel.TextScaled = true
+	TextLabel.TextWrapped = true
 	TextLabel.TextXAlignment = Enum.TextXAlignment.Left
 
 	local Window = {
